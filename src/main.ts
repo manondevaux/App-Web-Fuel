09460280e8af4c2ac555ed9aff2f06d2880fe581
Manon <manon.devaux@etu.emse.fr>
2024-12-05 01:44:30 +0100

    clever

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { FuelModule } from './fuel.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(FuelModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
